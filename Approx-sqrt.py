b78e88a4b55d94bb64281b719b8ffddc8e7c691d
Eoghan O'Connor <oconnorjohn40@gmail.com>
2019-09-25 19:15:29 +0100

    Changing Spelling error and adding print

--- Approx-sqrt.py
+++ Approx-sqrt.py
@@ -1,14 +1,16 @@
 # Adapted from https://tour.golang.org/flowcontrol/8
 
-def sprt(x):
+def sqrt(x):
     # Inital guess
     z = 1.0
     # Keep getting better est. for the square root of x, until you are within 2 decimal places 
     while abs(z*z - x) >= 0.01:
         # Get a better approximation for the square root 
-    z -= (z*z - x) / (2*z)
-
-
+        z -= (z*z - x) / (2*z)
     return z
 
-    sqrt(8.0)
+
+
+
+# Calculate the square root of 8
+print(sqrt(8.0))
